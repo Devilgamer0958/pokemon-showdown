5fddbd8609965045ccacd73256d5d17b198035e9
Devilgamer0958 <65742640+Devilgamer0958@users.noreply.github.com>
2020-05-28 21:45:04 +1000

    Update scripts.ts

--- data/mods/mixandmega/scripts.ts
+++ data/mods/mixandmega/scripts.ts
@@ -22,62 +22,6 @@ export const BattleScripts: ModdedBattleScriptsData = {
 		if (pokemon.species.isMega) return false;
 		},
 
-		if (pokemon.hasItem ('blueorb')) {
-			onSwitchIn(pokemon) {
-		if (pokemon.isActive && pokemon.baseSpecies.name === 'Kyogre') {
-			this.queue.insertChoice({choice: 'runPrimal', pokemon: pokemon});
-		 }
-	 },
-			onPrimal: (pokemon) {
-				/**@type {Template} */
-				// @ts-ignore
-				let template = this.getMixedTemplate(pokemon.originalSpecies, 'Kyogre-Primal');
-				if (pokemon.originalSpecies === 'Kyogre') {
-					pokemon.formeChange(template, this.effect, true);
-				} else {
-					pokemon.formeChange(template, this.effect, true);
-					pokemon.baseTemplate = template;
-					this.add('-start', pokemon, 'Blue Orb', '[silent]');
-				}
-			},
-			onTakeItem: function (item) {
-				return false;
-			},
-		},
-		if (pokemon.hasItem ('redorb')) {
-			onSwitchIn(pokemon) {
-		if (pokemon.isActive && pokemon.baseSpecies.name === 'Groudon') {
-			this.queue.insertChoice({choice: 'runPrimal', pokemon: pokemon});
-		 }
-	 },
-			onPrimal: function (pokemon) {
-				/**@type {Template} */
-				// @ts-ignore
-				let template = this.getMixedTemplate(pokemon.originalSpecies, 'Groudon-Primal');
-				if (pokemon.originalSpecies === 'Groudon') {
-					pokemon.formeChange(template, this.effect, true);
-				} else {
-					pokemon.formeChange(template, this.effect, true);
-					pokemon.baseTemplate = template;
-					this.add('-start', pokemon, 'Red Orb', '[silent]');
-					// @ts-ignore
-					let oTemplate = this.getTemplate(pokemon.illusion || pokemon.originalSpecies);
-					this.add('-start', pokemon, 'Red Orb', '[silent]');
-					if (pokemon.illusion) {
-						pokemon.ability = '';
-						let types = oTemplate.types;
-						if (types.length > 1 || types[types.length - 1] !== 'Fire') {
-							this.add('-start', pokemon, 'typechange', (types[0] !== 'Fire' ? types[0] + '/' : '') + 'Fire', '[silent]');
-						}
-					} else if (oTemplate.types.length !== pokemon.template.types.length || oTemplate.types[1] !== pokemon.template.types[1]) {
-						this.add('-start', pokemon, 'typechange', pokemon.template.types.join('/'), '[silent]');
-					}
-				}
-			},
-			onTakeItem: function (item) {
-				return false;
-			},
-		},
 
 		// @ts-ignore
 		const species: Species = this.getMixedSpecies(pokemon.m.originalSpecies, pokemon.canMegaEvo);
